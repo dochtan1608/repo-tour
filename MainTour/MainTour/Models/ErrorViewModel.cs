487793f35b466352be53cf2693d5f481937b4d70
dochtan1608 <89788071+dochtan1608@users.noreply.github.com>
2024-04-04 21:24:50 +0700

    first-commit

---
+++ MainTour/MainTour/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MainTour.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
